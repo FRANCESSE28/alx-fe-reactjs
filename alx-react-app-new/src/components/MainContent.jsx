89e429ff317e459cf5d6b42ebc8e128db0bdf175
Franca Zogli <francazogli282gmail.com>
2025-09-07 21:50:26 +0000

    Task 0: Applied inline CSS styling to components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f9f9f9', padding: '20px', minHeight: '100px' }}>
+      <p>Here is the main content of the app. You can add anything here!</p>
+    </main>
+  );
+}
+
+export default MainContent;

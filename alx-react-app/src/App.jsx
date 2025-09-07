3f3235c1971b8f5d32973179ca29deec3c911532
Franca Zogli <francazogli282gmail.com>
2025-09-07 20:30:54 +0000

    Task 3: Added UserProfile component with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
@@ -26,6 +27,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="David" age="30" bio="Enjoys coding and traveling" />
 
 
       <div className="card">

f190e6209ff670f6f571a7f4476e92cb981bc7d3
Franca Zogli <francazogli282gmail.com>
2025-10-09 17:17:27 +0000

    Setup Tailwind CSS and project dependencies

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind CSS is Working 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
